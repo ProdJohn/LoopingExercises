0100e97945a807857c23af63f09e21df3ed03dfd
ProdJohn <fwtx.john@gmail.com>
2024-10-03 13:14:48 -0500

    loop exercise first time practice

---
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class ForLoop {
+    public static void main(String[] args) throws InterruptedException {
+    for (int i = 10; i > 0; i--) {
+        System.out.println(i);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+        System.out.println("Launch!");
+    }
+}
